a48e0177fc3b6ed20ab01329ff11093792bcce72
YerbolIrgaliyev <yerbol.irgaliyev@gmail.com>
2019-04-07 23:47:30 +0600

    first commit

---
+++ Money.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Practice_10_01
+{
+    public class Money
+    {
+        const int DEFAULT_PAR = 1000;
+        const int DEFAULT_COUNT = 10;
+
+        public int Par { get; }
+        public int Count { get; set; }
+        public int Total {
+            get
+            {
+                return Par * Count;
+            }
+        }
+
+        public Money(int par, int count)
+        {
+            if(par <= 0)
+            {
+                Par = DEFAULT_PAR;
+            }
+            else Par = par;
+
+            if(Count <= 0)
+            {
+                Count = DEFAULT_COUNT;
+            }
+            else Count = count;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Номинал : {Par}");
+            Console.WriteLine($"Кол-во  : {Count}");
+        }
+
+        public bool CanBuy(int cost)
+        {
+            if(Par * Count >= cost)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int HowManyCanBuy(int cost)
+        {
+            return (Par * Count) / cost;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice_10_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int PRODUCT_COST = 1400;
+
+            Money money = new Money(0, 0);
+            if (money.CanBuy(PRODUCT_COST))
+            {
+                Console.WriteLine("Это можно купить");
+                Console.WriteLine($"Так же в кол-ве {money.HowManyCanBuy(PRODUCT_COST)} штук ");
+            }
+            else
+            {
+                Console.WriteLine("Не хватает денег!");
+                Console.WriteLine($"У вас {money.Total}, а нужно {PRODUCT_COST}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
